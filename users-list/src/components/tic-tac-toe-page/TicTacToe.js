c04eb37c302f3fc566ff6843611cff2a054e6b56
maria ryabikovskaya <niceshizachan@gmail.com>
2023-07-04 20:25:34 +0300

    feat: add tic-tac-toe page (without time travel)

---
+++ users-list/src/components/tic-tac-toe-page/TicTacToe.js
@@ -0,0 +1,97 @@
+import styles from './TicTacToe.module.css'
+import { useState } from 'react';
+
+const TicTacToe = () => {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick(i) {
+        if (squares[i] || calculateWinner(squares)) {
+            return;
+        }
+
+        const copySquares = squares.slice(); // array's copy
+        if (xIsNext) {
+            copySquares[i] = 'X';
+        }
+        else {
+            copySquares[i] = 'O';
+        }
+        setSquares(copySquares); // re-render
+        setXIsNext(!xIsNext);
+    }
+
+    function handleResetClick() {
+        setSquares(Array(9).fill(null))
+    }
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    }
+    else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
+    return (
+        <div className={styles.content}>
+            <div className={styles.status}>
+                {status}
+            </div>
+            <div className={styles.row}>
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+            </div>
+            <div className={styles.row}>
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+
+            </div>
+            <div className={styles.row}>
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+            </div>
+            <button className={styles.reset} onClick={() => handleResetClick()}>
+                Reset
+            </button>
+        </div>
+    )
+}
+
+const Square = ({ value, onSquareClick }) => {
+    return (
+        <button
+            className={styles.square}
+            onClick={onSquareClick}
+        >
+            {value}
+        </button>
+    )
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+export default TicTacToe
