fcf7a9b2115979ee2328e37ff2ad16834f44f592
maria ryabikovskaya <niceshizachan@gmail.com>
2023-07-04 20:24:45 +0300

    feat: add tic-tac-toe route

--- users-list/src/App.js
+++ users-list/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Contacts from './components/contacts-page/Contacts';
 import AboutUs from './components/about-us-page/AboutUs';
 import Header from './components/common/header/Header';
+import TicTacToe from './components/tic-tac-toe-page/TicTacToe';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Contacts />} />
           <Route path='/aboutus' element={<AboutUs />} />
+          <Route path='/tictactoe' element={<TicTacToe />} />
         </Routes>
       </main>
     </BrowserRouter>
