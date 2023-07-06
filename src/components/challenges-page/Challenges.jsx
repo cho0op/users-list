06b77674a330c876a173494e9ba11712666e307f
maria ryabikovskaya <niceshizachan@gmail.com>
2023-07-06 10:15:09 +0300

    feat: add updating arrays on challenges page

--- src/components/challenges-page/Challenges.jsx
+++ src/components/challenges-page/Challenges.jsx
@@ -1,10 +1,12 @@
 import styles from './Challenges.module.css'
 import UpdatingObjects from './updating-objects/UpdatingObjects'
+import UpdatingArrays from './updating-arrays/UpdatingArrays'
 
 const Challenges = () => {
     return (
         <div className={styles.content}>
             <UpdatingObjects />
+            <UpdatingArrays />
         </div>
     )
 }
