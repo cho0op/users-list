c6186e18f752cf60cd79e158708c9a0d85a3fc49
maria ryabikovskaya <niceshizachan@gmail.com>
2023-07-09 23:01:10 +0300

    feat: add form

---
+++ src/components/challenges-page/form/input/Input.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useRef } from "react";
+
+export default function Input({ value, onChange }) {
+  const ref = useRef(null);
+  useEffect(() => {
+    ref.current.focus();
+  }, []);
+
+  return <input ref={ref} value={value} onChange={onChange} />;
+}
