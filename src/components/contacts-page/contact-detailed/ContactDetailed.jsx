6da3d22fadc7c59856d453b9e2e169c2e9185a89
maria ryabikovskaya <niceshizachan@gmail.com>
2023-07-11 22:28:04 +0300

    feat: add detailed contact component

---
+++ src/components/contacts-page/contact-detailed/ContactDetailed.jsx
@@ -0,0 +1,49 @@
+import styles from './ContactDetailed.module.css';
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
+const ContactDetailed = () => {
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setInfo(data);
+            })
+            .catch((error) => {
+                console.error('Error: ', error);
+            });
+    }, []);
+
+    const { id } = useParams();
+    const [info, setInfo] = useState({
+        address: { city: '' },
+        company: { name: '' },
+    });
+    let navigate = useNavigate();
+
+    return (
+        <div className={styles.content}>
+            <div className={styles.rect}>
+                <div className={styles.name}>
+                    {info.name} aka "{info.username}"
+                </div>
+                <div className={styles.phone}>Phone number: {info.phone}</div>
+                <div className={styles.email}>E-mail: {info.email}</div>
+                <div className={styles.address}>City: {info.address.city}</div>
+                <div className={styles.company}>
+                    Company: {info.company.name}
+                </div>
+            </div>
+            <button
+                className={styles.goback_button}
+                onClick={() => {
+                    navigate('/');
+                }}
+            >
+                Go back
+            </button>
+        </div>
+    );
+};
+
+export default ContactDetailed;
