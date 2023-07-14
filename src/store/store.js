8b0579a88c200764bb6f91ef5c737d0321b99f74
maria ryabikovskaya <niceshizachan@gmail.com>
2023-07-14 14:44:30 +0300

    feat: add contact slice to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import contactsSlice from '../components/contacts-page/contacts-redux-store/contactsSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import contactsSlice from '../components/contacts-page/contacts-redux-store/contactsSlice';
+import contactSlice from '../components/contacts-page/contacts-redux-store/contact-detailed/contactDetailedSlice';
 
 export const store = configureStore({
-  reducer: {
-    contacts: contactsSlice
-  },
-})
+    reducer: {
+        contacts: contactsSlice,
+        contact: contactSlice,
+    },
+});
